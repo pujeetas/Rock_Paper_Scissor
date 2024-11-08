b93b7fdf7d93ef1f43b91157c4bcb013e7858785
pujeetas <pujeeta98@gmail.com>
2024-11-08 23:20:54 +0530

    Added Code Comment

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
-class Main{
+class Main {
+
+    /**
+     * Main method to start the Rock-Paper-Scissors game.
+     * Creates an instance of the Game class and initiates the checkWinner method,
+     * which manages the game rounds and determines the final winner.
+     */
     public static void main(String[] args) {
 
-    Game game = new Game();
-    game.checkWinner();
+        Game game = new Game(); // Create a Game object
+        game.checkWinner(); // Start the game and determine the final winner
     }
 }
